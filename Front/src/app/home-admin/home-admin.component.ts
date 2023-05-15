f9efd89f400fc0f589c820603aac372e0cde2899
nicholasstelmach <nicholasstelmach@icloud.com>
2023-05-15 10:11:47 -0300

    Arrumando

--- Front/src/app/home-admin/home-admin.component.ts
+++ Front/src/app/home-admin/home-admin.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Route, Router } from '@angular/router';
+import axios from 'axios';
+import { User } from '../user';
 
 
 @Component({
@@ -9,11 +11,53 @@ import { Route, Router } from '@angular/router';
 })
 export class HomeAdminComponent implements OnInit {
 
+  user : User
+  userId : number;
+  
   constructor(private router: Router) {
     let self = this;
+
+    this.userId = 0;
+
+    this.user = {
+      id : this.userId,
+      nome : "",
+      edv: "",
+      senha: "",
+      area: "",
+      email: "",
+      dataNasc: ""
+    }
    }
 
   ngOnInit(): void {
+
+    let token = localStorage.getItem('authMedico');
+
+    var data = JSON.stringify({
+      
+    });
+    console.log(token)
+    let self = this;
+    var config2 = {
+      method: 'get',
+      url: 'http://localhost:5051/medico/getById',
+      headers: { 
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token
+      },
+      data : data
+    };
+
+    axios(config2)
+    .then(function (response:any) {
+      console.log(JSON.stringify(response.data));
+      self.userId = response.data;
+      self.user = response.data
+    })
+    .catch(function (error:any) {
+      console.log(error);
+    });
   }
   ocorrenciahistorico(){
     let self = this
@@ -21,10 +65,15 @@ export class HomeAdminComponent implements OnInit {
   }
   agenda(){
     let self = this
-    self.router.navigate(['agendalist']);
+    self.router.navigate(['agendacalendar']);
   }
   consultas(){
     let self = this
     self.router.navigate(['consultaslist']);
+  }
+
+  profile(){
+    let self = this
+    self.router.navigate(['profile']);
   }
 }
